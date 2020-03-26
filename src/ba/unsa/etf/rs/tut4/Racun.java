1fb4f95ab7f440be7ab32818a408748b20aaf812
Amer Beso <abeso1@etf.unsa.ba>
2020-03-26 12:46:42 +0100

    Uraden zadatak 3, dodani dodajStavku i ukupanIznos, svi testovi prolaze

--- src/ba/unsa/etf/rs/tut4/Racun.java
+++ src/ba/unsa/etf/rs/tut4/Racun.java
@@ -1,4 +1,21 @@
 package ba.unsa.etf.rs.tut4;
 
+import java.util.ArrayList;
+
 public class Racun {
+    private ArrayList<Artikal> artikli=new ArrayList<>();
+    private ArrayList<Integer> kolicine=new ArrayList<>();
+
+    public Racun(){}
+
+    public void dodajStavku(Artikal artikal, int i) {
+        artikli.add(artikal);
+        kolicine.add(i);
+    }
+
+    public double ukupanIznos() {
+        double suma=0;
+        for(int i=0; i<artikli.size(); i++) suma+=artikli.get(i).getCijena()*kolicine.get(i);
+        return suma;
+    }
 }
